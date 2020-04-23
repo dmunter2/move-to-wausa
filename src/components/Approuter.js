af5ad77b972d9975533f30ad1a36fb709b677d12
dmunter2 <49967133+dmunter2@users.noreply.github.com>
2020-04-23 04:19:50 -0700

    styles

--- src/components/Approuter.js
+++ src/components/Approuter.js
@@ -5,16 +5,19 @@ import Container from './Container';
 import Header from './Header'
 import News from './News';
 import Job from './Job';
+import Newheader from './Newheader';
+import Board from './Board';
 
 
 function Approuter() {
 
     return (
         <div>
+            <Route path='/involved' component={Newheader} />
             <Route path='/(|openings|news|business|food)' component={Header} />
             <Route path='/news' component={News} />
             <Route path='/openings' component={Job} />
-
+            <Route exact path='/involved' component={Board} />
             <Route path='/business' component={Listofbiz}  />
             <Route exact path='/' component={Container} />
 
--- src/components/Container.js
+++ src/components/Container.js
@@ -14,9 +14,9 @@ display: flex;
 flex-direction: column;
 overflow: auto;
 
-@media(min-width: 800px) {
-    display: none;
-}
+// @media(min-width: 800px) {
+//     display: none;
+// }
 
 `
 
@@ -34,6 +34,7 @@ background-color: white;
  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
  padding: 10px 0 30px 0;
  margin-bottom: 40px;
+ max-width: 500px;
 `
 
 
@@ -42,16 +43,16 @@ const DIV2 = style.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-@media(min-width: 800px) {
-    display: none;
-}
+// @media(min-width: 800px) {
+//     display: none;
+// }
 
 `
 
 const Other = style.div`
-@media(min-width: 800px) {
-    display: none;
-}
+// @media(min-width: 800px) {
+//     display: none;
+// }
 `
 const MAIN = style.div`
 margin-top: 3%;
@@ -59,12 +60,25 @@ display: flex;
 flex-direction: column;
 justify-content: space-around;
 width: 100%;
+
 `
 const H1 = style.h1`
-text-align: left;
 margin-left: 4%;
 font-size: 1.3rem;
 color: #25282a;
+text-align: left;
+
+
+`
+const H3 = style.h2`
+margin: 0;
+text-align: left;
+color: #1f2020;
+font-size: 1.8rem;
+fontweight: 800;
+margin-left: 20px;
+margin-top: 10px;
+height: 50px;
 `
 const H2 = style.h1`
 font-size: 1.6rem;
@@ -78,27 +92,55 @@ const DIV3 = style.div`
 display: flex;
 flex-direction: column;
 align-items: center;
+border-radius: 5px 5px 0 0;
+height: 250px;
 
 `
 const DIV4 = style.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-
+border-radius: 5px 5px 0 0;
+height: 250px;
+// justify-content: end;
 
 `
 const Contain1 = style.div`
-width: 100%;
+width: 92%;
 background-color: white;
   box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
   margin-bottom: 30px;
+  border-radius: 5px;
+  height: 320px;
+  max-width: 500px;
 `
+const Contain2 = style.div`
+box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+width: 92%;
+margin-bottom: 50px;
+height: 320px;
+max-width: 500px;
+margin-top: 15px;
+`
+
+
 const Img = style.img`
 height: 280px;
 width: 350px;
 `
-
-
+const DIV5 = style.div`
+width: 100%;
+`
+const H5 = style.h2`
+margin: 0;
+text-align: left;
+color: #1f2020;
+font-size: 1.8rem;
+fontweight: 800;
+margin-left: 20px;
+margin-top: 10px;
+height: 100px;
+`
 function Container(){
 
     return(
@@ -129,20 +171,33 @@ function Container(){
             </Contain>
 
             <Contain1 >
-                <H1>Grab Some Lunch</H1>
                 <DIV4 className='div4'>
-                    <Link className='tabs' to='/specials'>Daily Specials</Link>
-                    <Link className='tabs' to='/processing'>Custom Processing</Link>
+                    <Link className='lunch' to='/specials'>Daily Specials</Link>
+                    <Link className='lunch' to='/processing'>Custom Processing</Link>
+             
+
                 </DIV4>
-                <H1>The Handymen of Wausa</H1>
-                <DIV3>
-                    <Link className='tabs' to='/specials'>Burns Construction</Link>
-                    <Link className='tabs' to='/processing'>McQuay Construction</Link>
-                    <Link className='tabs' to='/plumbing'>Bloomquist Plumbing</Link>
-                </DIV3>
+                <DIV5>
+                    <H3>Grab some lunch</H3>
+
+                </DIV5>
+
+           
 
 
             </Contain1>
+
+            <Contain2>
+                
+                <DIV3 className='build'>
+
+                    <H5>Businesses Keeping Wausa Looking New</H5>
+                    <Link className='tabs5' to='/specials'>Thank you!</Link>
+
+
+                </DIV3>
+
+            </Contain2>
 
 
 
